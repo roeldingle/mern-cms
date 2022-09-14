8a921d3a7ea6c0fcde7bf9e87978b37785e90e46
roeldingle <roeldingle@gmail.com>
2022-09-14 18:17:40 +0800

    API CRUD completed

--- api/controllers/userControllers.js
+++ api/controllers/userControllers.js
@@ -1,10 +1,14 @@
 const asyncHandler = require('express-async-handler')
+const bcrypt = require('bcrypt')
+
+const User = require('../models/userModels')
 
 //@desc Get all users
 //@route GET/api/users
 //@access Private
 const getUsers = asyncHandler(async(req,res) => {
-    res.status(200).json({message: 'all users here'})
+    const user = await User.find()
+    res.status(200).json({message: 'All user data', data: user})
 })
 
 //@desc Create user
@@ -22,8 +26,7 @@ const createUser = asyncHandler(async(req,res) => {
 
     /*check sent data if available*/
     if(
-        !role
-        || !firstname 
+        !firstname 
         || !lastname 
         || !email 
         || !password
@@ -32,23 +35,61 @@ const createUser = asyncHandler(async(req,res) => {
         throw new Error('Missing field data');
     }
 
-    res.status(200).json({message: 'Create a user here'})
+    //Check duplicate
+    const hasDuplicate = await User.findOne({email}).lean().exec()
+
+    if(hasDuplicate){
+        return res.status(400).json({message: 'Duplicate email'})
+    }
+
+    //Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10) //salt rounds
+
+    const userObject = {firstname, lastname, email, password: hashedPassword, role}
+
+    //Craete and store new user
+    const user = await User.create(userObject)
+
+    if(user){
+        return res.status(200).json({message: `New user ${email} created`, data: user})
+    }else{
+        return res.status(400).json({message: 'Invalid user data received'})
+    }
 })
 
 //@desc Update user
 //@route PUT/api/users/:id
 //@access Private
 const updateUser = asyncHandler(async(req,res) => {
-    res.status(200).json({message: `Update user ${req.params.id} here`})
+
+    const userToUpdate = await User.findById(req.params.id)
+    /*check if user exist*/
+    if(!userToUpdate){
+        res.status(400);
+        throw new Error('User not found');
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {new: true})
+
+    res.status(200).json({message: `Update user ${req.params.id} here`, data: updatedUser})
 })
 
 //@desc Delete user
 //@route DELETE/api/users/:id
 //@access Private
 const deleteUser = asyncHandler(async(req,res) => {
-    res.status(200).json({message: `Delete user ${req.params.id} here`})
-})
 
+    const userToDelete = await User.findById(req.params.id)
+    /*check if user exist*/
+    if(!userToDelete){
+        res.status(400);
+        throw new Error('User not found');
+    }
+
+    await userToDelete.remove()
+
+    res.status(200).json({message: `Delete user ${req.params.id} here`, data: req.params.id})
+})
 
 module.exports = {
     getUsers,
---
+++ api/models/userModels.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    firstname:{
+        type: String,
+        required: true
+    },
+    lastname:{
+        type: String,
+        required: true
+    },
+    email: {
+        type:String,
+        required: [true, 'Please add an email'],
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    role:{
+        type: String,
+        default: "member"
+    },
+    active:{
+        type: Boolean,
+        default: true
+    },
+},{
+    autoIndex: true,
+    timestamps: true
+})
+
+module.exports = mongoose.model('User', userSchema)
